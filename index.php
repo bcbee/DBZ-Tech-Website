664640d1c298d370babf9cee35ec4d9d7ba12507
Brendan Boyle <brendancboyle@me.com>
2013-03-01 09:34:21 -0600

    Added first view

--- index.php
+++ index.php
@@ -9,9 +9,9 @@ include('classes.php');
     </head>
     <body>
         <div class="view" id="view1">
-            <div class="panel" id="left1">
-            </div>
-            <div class="panel" id="right1">
+            <div class="logo">
+                <h1>DBZ</h1>
+                <h2>Technology</h2>
             </div>
         </div>
     </body>
