372213b4a4607fd5cf9db31cc174d1088a84443e
Brendan Boyle <brendancboyle@me.com>
2013-03-02 13:07:22 -0600

    Added panel support and tweaked css

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ include('classes.php');
     <head>
         <link rel="stylesheet" href="styles.css" />
         <script src="scripts.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     </head>
     <body>
         <div class="view" id="view1">
@@ -18,7 +19,23 @@ include('classes.php');
             </div>
         </div>
         <div class="view" id="view2">
-            <div class="panel"></div>
+            <div class="panel">
+                <div class="panelrect">
+                    <div class="panelpadding">
+                        <p>This is text</p>
+                    </div>
+                </div>
+                <img src="Resources/Images/PanelCorner.svg" class="panelcorner" alt="Corner" />
+            </div>
+            <div class="ribbon"></div>
+            <!--<img src="Resources/Images/arrow.svg" alt="Continue" class="arrow downarrow" id="arrow1" />-->
+        </div>
+        <div class="view" id="view3">
+            <div class="ribbon"></div>
+            <img src="Resources/Images/arrow.svg" alt="Continue" class="arrow downarrow" id="arrow1" />
+        </div>
+        <div class="view" id="view4">
+            <div class="ribbon"></div>
             <img src="Resources/Images/arrow.svg" alt="Continue" class="arrow downarrow" id="arrow1" />
         </div>
     </body>
