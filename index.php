b596ec7f17ef32d76766ef1597f97af788015fb7
Brendan Boyle <brendan@dbztech.com>
2013-03-15 16:48:23 -0500

    Updated index

--- index.php
+++ index.php
@@ -24,6 +24,7 @@ include('classes.php');
                     <div class="panelpadding">
                         <h1>Compelling Design</h1>
                         <h2>Simple. Flexible. Elegant.</h2>
+                        <p>Design is at the core of what we do at DBZ Technology. We focus heavily on user experience and interaction to make sure your organization's new website is easy to understand and use both on a user and administrator standpoint. Once you purchase a DBZ Technology designed website we update it for free. We strive to make your organization known and we strongly and thoroughly incorporate your brand in everything we create. Don't have a brand? No problem! DBZ Technology is more than happy to work with your organization to develop strong branding and design elements for future products. Once we create a brand we make sure you get the vector files, colors, and logos so that you can develop your brand later with no haste.</p>
                     </div>
                 </div>
                 <img src="Resources/Images/PanelCorner.svg" class="panelcorner" alt="Corner" />
@@ -36,7 +37,8 @@ include('classes.php');
                 <div class="panelrect">
                     <div class="panelpadding">
                         <h1>Consulting</h1>
-                        <h2>Affortible Consulting for Computing Solutions</h2>
+                        <h2>Affordable Consulting for Computing Solutions</h2>
+                        <p>DBZ Technology also specializes in end to end product solutions. Weather your computer needs a quick tune up or you need a web application designed to handle massive amounts of data, DBZ Technology is always there for support. We work with you one on one to ensure that our solution fits your needs perfectly. Let's say that you need a new IT infrastructure as cheaply as possible. No Problem! DBZ Technology can analyze your current situation, make a plan, build new computers for almost 50% off retail, make the switch in under 3 days, and transfer all of your information to the new system.</p>
                     </div>
                 </div>
                 <img src="Resources/Images/PanelCorner2.svg" class="panelcorner panelcornerleft" alt="Corner" />
@@ -50,10 +52,10 @@ include('classes.php');
             <div class="ribbon" style="left: 0px; height: 50px;"></div>
             <div class="ribbon horizontal"><div class="block" style="width: 283px; background: white;"><img src="Resources/Images/FoldRight.svg" alt="Fold Right" style="margin-left: 83px" /></div></div>
             <div class="buttonholder">
-                <div class="button smallbutton"></div>
-                <div class="button smallbutton"></div>
-                <div class="button smallbutton"></div>
-                <div class="button bigbutton"><p>@Contact</p></div>
+                <a class="button smallbutton" href="#"></a>
+                <a class="button smallbutton" href="#"></a>
+                <a class="button smallbutton" href="#"></a>
+                <a class="button bigbutton" href="#"><p>@Contact</p></a>
             </div>
             <!--<img src="Resources/Images/arrow.svg" alt="Continue" class="arrow downarrow" id="arrow1" />-->
         </div>
