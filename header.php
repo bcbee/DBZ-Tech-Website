e8320aba2307dd8d28c6ffaefe405eca1c6505ea
Brendan Boyle <brendancboyle@me.com>
2013-03-25 14:16:47 -0700

    Added analytics and robots.txt

--- header.php
+++ header.php
@@ -5,6 +5,18 @@
         <script src="scripts.js"></script>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <title><?php echo 'DBZ Technology | '.$title; ?></title>
+        <script type="text/javascript">
+          var _gaq = _gaq || [];
+          _gaq.push(['_setAccount', 'UA-21351328-1']);
+          _gaq.push(['_trackPageview']);
+        
+          (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+          })();
+        
+        </script>
     </head>
     <body>
         <div class="header">
