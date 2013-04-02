54f8d981b1b733119b443191785b741e57d722ce
Brendan Boyle <brendancboyle@me.com>
2013-04-02 13:18:47 -0500

    New SPLAT logo

--- Cards/Splat.php
+++ Cards/Splat.php
@@ -1,3 +1,3 @@
 <h1>SPLAT Charity</h1>
-<img src="/Resources/Images/Splat.png" alt="SPLAT" style="width: 180px; margin-bottom: 50px;" />
+<img src="/Resources/Images/SPLAT.svg" alt="SPLAT" style="width: 180px; margin-bottom: 50px;" />
 <a class="button longbutton" href="http://splatcharity.org" style="background: white; color: red;">View Website</a>
