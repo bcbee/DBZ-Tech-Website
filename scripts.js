9638cf0633ab87fe2901c754608a22eded00a47c
Brendan Boyle <brendancboyle@me.com>
2013-04-21 00:26:26 -0500

    Removed script

--- scripts.js
+++ scripts.js
@@ -1,7 +1,1 @@
-//Scroll code: var vscroll = (document.all ? document.scrollTop : window.pageYOffset);
-
-window.onscroll = function (oEvent) {
-    // called when the window is scrolled.
-    vscroll = (document.all ? document.scrollTop : window.pageYOffset);
-    //console.log(vscroll);
-}
+//JavaScript Document
