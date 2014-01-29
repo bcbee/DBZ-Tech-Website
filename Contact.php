347473fe1de07ae52f1fed708629f9d302998016
Brendan Boyle <brendancboyle@me.com>
2014-01-29 08:54:35 -0600

    Fixed styling and added base contact page

--- Contact.php
+++ Contact.php
@@ -1,8 +1,29 @@
 <?php
-if(mail('user@mydomain.com','test subject','test message')){
-      echo('ok');
-    }
-else{
-      echo('not ok');
-    }
+include('classes.php');
+$title = "Consulting";
+$back = "/#view3";
+include('header.php');
+?>
+
+<div id="contact">
+    <form method="post" role="form" id="contactform" action="/Contact">
+        <div class="btn-group">
+          <button type="button" class="btn btn-success">Request a Quote</button>
+          <button type="button" class="btn btn-success">Ask a Qeustion</button>
+          <button type="button" class="btn btn-success">Other</button>
+        </div>
+        <br />
+        <br />
+        <div class="input-group"><span class="input-group-addon">Name</span><input type="text" name="name" class="form-control" required/></div>
+        <br />
+        <div class="input-group"><span class="input-group-addon">Email</span><input type="text" name="from" class="form-control" required/></div>
+        <h2>Message:</h2>
+        <textarea class="form-control" name="message" rows="6" required></textarea>
+        <br />
+        <input class="form-control btn btn-success" type="submit" style="width: 200px" />
+    </form>
+</div>
+
+<?php
+include('footer.php');
 ?>
