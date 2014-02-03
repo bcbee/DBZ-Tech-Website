866aa36dbc8ca65daed2f8c83b95762cb062f506
Brendan Boyle <brendancboyle@me.com>
2014-02-02 21:57:35 -0600

    Change Resume spelling

--- Cards/Qualifications.php
+++ Cards/Qualifications.php
@@ -1,4 +1,4 @@
 <h1>Qualifications</h1>
 <h2>Languages:</h2>
 <p>PHP, HTML 5, CSS 3, JavaScript, MySQL, Java, C (#, ++, Objective)</p>
-<a class="btn btn-success longbutton" href="/Resources/Files/Resume.pdf">Resum&eacute;</a>
+<a class="btn btn-success longbutton" href="/Resources/Files/Resume.pdf">R&eacute;sum&eacute;</a>
