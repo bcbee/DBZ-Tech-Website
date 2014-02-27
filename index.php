c757989d08f51cf9934aeeb777f00de9b5bc6c85
Brendan Boyle <brendancboyle@me.com>
2014-02-26 23:11:41 -0600

    Fixed CSS bug with contact page

--- index.php
+++ index.php
@@ -62,8 +62,8 @@ include('classes.php');
                 <div class="panelrect" id="panel2">
                     <div class="panelpadding">
                         <a href="/Consulting" class="link"><img src="/Resources/Images/Link.svg" alt="Link" /></a>
-                        <h1>Consulting</h1>
-                        <h2>Affordable Consulting for Computing Solutions</h2>
+                        <h1>Technology Experts</h1>
+                        <h2>Website Development, Programming, and Computer consulting one click away</h2>
                         <p>DBZ Technology also specializes in end to end product solutions. Weather your computer needs a quick tune up or you need a web application designed to handle massive amounts of data, DBZ Technology is always there for support. We work with you one on one to ensure that our solution fits your needs perfectly. Let's say that you need a new IT infrastructure as cheaply as possible. No Problem! DBZ Technology can analyze your current situation, make a plan, build new computers for almost 50% off retail, make the switch in under 3 days, and transfer all of your information to the new system.</p>
                     </div>
                 </div>
