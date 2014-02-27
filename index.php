12d0bcbc26df965b297c34580acf42feea9deeac
Brendan Boyle <brendancboyle@me.com>
2014-02-27 10:49:19 -0600

    Index interface improvements

--- index.php
+++ index.php
@@ -46,25 +46,24 @@ include('classes.php');
             <div class="dbzpanel">
                 <div class="panelrect" id="panel1">
                     <div class="panelpadding">
-                        <a href="/Design" class="link"><img src="/Resources/Images/Link.svg" alt="Link" /></a>
                         <h1>Compelling Design</h1>
                         <h2>Simple. Flexible. Elegant.</h2>
                         <p>Design is at the core of what we do at DBZ Technology. We focus heavily on user experience and interaction to make sure your organization's new website is easy to understand and use both on a user and administrator standpoint. Once you purchase a DBZ Technology designed website we update it for free. We strive to make your organization known and we strongly and thoroughly incorporate your brand in everything we create. Don't have a brand? No problem! DBZ Technology is more than happy to work with your organization to develop strong branding and design elements for future products. Once we create a brand we make sure you get the vector files, colors, and logos so that you can develop your brand later with no haste.</p>
+                        <a href="/Design" class="btn btn-success viewbutton"><span class="glyphicon glyphicon-pencil roundbuttonicon"></span><br />Design</a>
                     </div>
                 </div>
                 <img src="Resources/Images/PanelCorner.svg" class="panelcorner" alt="Corner" />
             </div>
             <div class="ribbon"></div>
-            <!--<img src="Resources/Images/arrow.svg" alt="Continue" class="arrow downarrow" id="arrow1" />-->
         </div>
         <div class="view" id="view3">
             <div class="dbzpanel panelleft" style="">
                 <div class="panelrect" id="panel2">
                     <div class="panelpadding">
-                        <a href="/Consulting" class="link"><img src="/Resources/Images/Link.svg" alt="Link" /></a>
                         <h1>Technology Experts</h1>
                         <h2>Website Development, Programming, and Computer consulting one click away</h2>
                         <p>DBZ Technology also specializes in end to end product solutions. Weather your computer needs a quick tune up or you need a web application designed to handle massive amounts of data, DBZ Technology is always there for support. We work with you one on one to ensure that our solution fits your needs perfectly. Let's say that you need a new IT infrastructure as cheaply as possible. No Problem! DBZ Technology can analyze your current situation, make a plan, build new computers for almost 50% off retail, make the switch in under 3 days, and transfer all of your information to the new system.</p>
+                        <a href="/Technology" class="btn btn-success viewbutton"><span class="glyphicon glyphicon-flash roundbuttonicon"></span><br />Technology</a>
                     </div>
                 </div>
                 <img src="Resources/Images/PanelCorner2.svg" class="panelcorner panelcornerleft" alt="Corner" />
@@ -72,7 +71,6 @@ include('classes.php');
             <div class="ribbon" style="height:50px;"></div>
             <div class="ribbon" style="left: 0px; margin-top: 250px;"></div>
             <div class="ribbon horizontal" style=""><div class="block" style="width: 283px;"><img src="Resources/Images/FoldDown.svg" alt="Fold Down" style="margin-left: 83px" /></div><div class="block" style="width: 283px; float: right; margin-top: -200px;"><img src="Resources/Images/FoldLeft.svg" alt="Fold Left" style="margin-right: 83px" /></div></div>
-            <!--<img src="Resources/Images/arrow.svg" alt="Continue" class="arrow downarrow" id="arrow1" />-->
         </div>
         <div class="view" id="view4">
             <div class="ribbon" style="left: 0px; height: 50px;"></div>
