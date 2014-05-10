4676d6eb5c1120d4f65ab0e4cbd001496750b84d
Brendan Boyle <brendancboyle@me.com>
2014-05-10 01:17:30 -0500

    Added title cards and new styles

--- classes.php
+++ classes.php
@@ -3,15 +3,22 @@
 include('Search/search.php');
 
 class cards {
-    public static function loadCards($cardsToLoad) {
+    public static function loadCards($cardsToLoad, $type) {
         foreach($cardsToLoad as $currentCard) {
             $filename = "Cards/".$currentCard.".php";
-            echo '<div class="card" id="'.$currentCard.'">';
-            #echo $filename;
-            if (file_exists($filename)) {
-                include($filename);
-    	    }
-            echo '</div>';
+            if ($type == 'title') {
+                echo '<div class="card titlecard" id="'.$currentCard.'">';
+                if (file_exists($filename)) {
+                    include($filename);
+                }
+                echo '</div><br />';
+            } else {
+                echo '<div class="card" id="'.$currentCard.'">';
+                if (file_exists($filename)) {
+                    include($filename);
+                }
+                echo '</div>';
+            }
         }
     }
 }
@@ -24,33 +31,33 @@ class portfolio {
         
         switch(portfolio::$view) {
             case "All":
-            $row1 = array('Splat', 'MPAror', 'Erickson');
-            $row2 = array('CreateOldServer', 'CreateServer', 'You');
-            $row3 = array('CreateBrock', 'CreateGrandpa');
+            $detailcards = array('Splat', 'MPAror', 'UPR', 'Erickson', 'CreateOldServer', 'CreateServer', 'You', 'CreateBrock', 'CreateGrandpa');
             $viewfull = false;
             break;
             case "Design":
-            $row1 = array('Design');
-            $row2 = array('Splat', 'MPAror');
-            $row3 = array('You');
+            $titlecard = array('Design');
+            $detailcards = array('Splat', 'MPAror');
+            $row3 = array();
             break;
             case "Technology":
-            $row1 = array('Technology', 'CreateServer');
-            $row2 = array('CreateBrock', 'CreateOldServer', 'CreateGrandpa');
-            $row3 = array('Erickson', 'You');
+            $titlecard = array('Technology');
+            $detailcards = array('CreateServer', 'CreateBrock', 'CreateOldServer', 'CreateGrandpa', 'Erickson');
             break;
             case "Innovation":
-            $row1 = array('ComingSoon', 'You');
+            $detailcards = array('ComingSoon');
             break;
             case "Apps":
-            $row1 = array('UPR', 'You');
+            $detailcards = array('UPR');
             break;
             case "Profile":
-            $row1 = array('Bio','Qualifications');
-            $row2 = array('Portfolio','Twitter');
+            $detaildards = array('Bio','Qualifications','Portfolio','Twitter');
             break;
             default:
-            $row1 = array('ComingSoon', 'You');
+            $detailcards = array('ComingSoon');
+        }
+        
+        if (isset($titlecard)) {
+            cards::loadCards($titlecard, 'title');
         }
         
         
@@ -58,21 +65,14 @@ class portfolio {
             echo '<div style="width: 100%; height: 50px;"><a class="btn btn-success longbutton" href="/Portfolio"><span class="glyphicon glyphicon-user" style="margin-right: 5px;"></span>View Full Portfolio</a></div>';
         }
         
-        if (isset($row1)) {
-            echo '<div class="cardrow">';
-            cards::loadCards($row1);
-            echo '</div>';
+        array_push($detailcards, 'You');
+        
+        
+        if (isset($detailcards)) {
+            cards::loadCards($detailcards, 'detail');
         }
-        if (isset($row2)) {
-            echo '<div class="cardrow">';
-            cards::loadCards($row2);
-            echo '</div>';
-        }
-        if (isset($row3)) {
-            echo '<div class="cardrow">';
-            cards::loadCards($row3);
-            echo '</div>';
-        }
+        
+        
     }
 }
 
