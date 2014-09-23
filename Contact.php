6f13f2db3c8c6992efb975ced10f3dfc33a2918b
Brendan Boyle <brendancboyle@me.com>
2014-09-22 22:37:05 -0400

    New contact form

--- Contact.php
+++ Contact.php
@@ -9,22 +9,26 @@ include('header.php');
 <div id="contact">
     <h1 class="pagetitle">Contact DBZ Technology</h1>
     
-    <p class="btn btn-success supportbutton"><span class="glyphicon glyphicon-thumbs-up"></span> Support</p>
-    <p class="btn btn-success supportbutton"><span class="glyphicon glyphicon-usd"></span> Request a Quote</p>
-    <p class="btn btn-success supportbutton"><span class="glyphicon glyphicon-question-sign"></span> Ask a Question</p>
-    <p class="btn btn-success supportbutton"><span class="glyphicon glyphicon-info-sign"></span> Other</p>
+    <p class="btn btn-success supportbutton" onclick="Core.hideId('supportform'); Core.hideId('genericform'); Core.showId('supportform');"><span class="glyphicon glyphicon-thumbs-up"></span> Support</p>
+    <p class="btn btn-success supportbutton" onclick="Core.hideId('supportform'); Core.hideId('genericform'); Core.showId('genericform'); document.getElementById('genericformsubject').value = 'Request a Quote';  document.getElementById('genericformheader').innerHTML = 'Request a Quote';"><span class="glyphicon glyphicon-usd"></span> Request a Quote</p>
+    <p class="btn btn-success supportbutton" onclick="Core.hideId('supportform'); Core.hideId('genericform'); Core.showId('genericform'); document.getElementById('genericformsubject').value = 'Ask a Question';  document.getElementById('genericformheader').innerHTML = 'Ask a Question';"><span class="glyphicon glyphicon-question-sign"></span> Ask a Question</p>
+    <p class="btn btn-success supportbutton" onclick="Core.hideId('supportform'); Core.hideId('genericform'); Core.showId('genericform'); document.getElementById('genericformsubject').value = 'Other';  document.getElementById('genericformheader').innerHTML = 'Other';"><span class="glyphicon glyphicon-info-sign"></span> Other</p>
     
     <br />
     <br />
     
-    <form method="post" role="form" id="contactform" action="/Contact">
-        <select class="form-control" name="subject" required>
-          <option>Request a Quote</option>
-          <option>Ask a Question</option>
-          <option>Other</option>
-        </select>
-        <br />
-        <br />
+    <form method="post" role="form" class="contactform" id="supportform" action="/Contact">
+        <script type="text/javascript" src="https://s3.amazonaws.com/assets.freshdesk.com/widget/freshwidget.js"></script>
+        <style type="text/css" media="screen, projection">
+            @import url(https://s3.amazonaws.com/assets.freshdesk.com/widget/freshwidget.css); 
+        </style> 
+        <iframe class="freshwidget-embedded-form" id="freshwidget-embedded-form" src="https://dbztechnology.freshdesk.com/widgets/feedback_widget/new?&widgetType=embedded&formTitle=DBZ+Technology+Support&submitThanks=We+are+incredibly+sorry+for+the+inconvenience%2C+an+agent+will+contact+you+within+two+business+days.&screenshot=no&searchArea=no" scrolling="no" height="400px" width="100%" frameborder="0" style="background: white; border-radius: 5px;" >
+        </iframe>
+    </form>
+    
+    <form method="post" role="form" class="contactform" id="genericform" action="/Contact">
+        <input type="hidden" name="subject" id="genericformsubject" value="Generic" />
+        <h1 id="genericformheader"></h1>
         <div class="input-group"><span class="input-group-addon">Name</span><input type="text" name="name" class="form-control" required/></div>
         <br />
         <div class="input-group"><span class="input-group-addon">Email</span><input type="email" name="from" class="form-control" required/></div>
--- JS/scripts.js
+++ JS/scripts.js
@@ -11,3 +11,26 @@ function changePage(title) {
 function mobilenavselect() {
     changePage(document.getElementById('mobilenav').value);
 }
+
+function core() {
+    this.showId = function(id) {
+        try {
+           document.getElementById(id).style.display = "block";
+        }
+        catch (e) {
+           // statements to handle any exceptions
+        }
+    }
+    
+    this.hideId = function(id) {
+        try {
+           document.getElementById(id).style.display = "none";
+        }
+        catch (e) {
+           // statements to handle any exceptions
+        }
+        
+    }
+}
+
+Core = new core;
