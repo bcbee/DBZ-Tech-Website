a28caac7307d8fa2dbb58a2057e91dfa7eddcdcd
Brendan Boyle <brendancboyle@me.com>
2014-10-06 00:33:21 -0400

    Added application insights

--- index.php
+++ index.php
@@ -14,6 +14,25 @@
         <meta name="msvalidate.01" content="59E1BC42B9D918F982BBB8F8BF6E4F6F" />
         <title>DBZ Technology | Home</title>
         <?php include('pingdom.php'); ?>
+        <!--
+        To collect end-user usage analytics about your application,
+        insert the following script into each page you want to track.
+        Place this code immediately before the closing </head> tag,
+        and before any other scripts. Note that it may take up to 15
+        minutes to process and display information here after
+        instrumenting and opening your web page.
+        -->
+        <script type="text/javascript">
+            window.appInsights=window.appInsights||function(ai){
+                function f(t){ai[t]=function(){var i=arguments;ai.queue.push(function(){ai[t].apply(ai,i)})}}
+                var t=document,r="script",u=t.createElement(r),i;for(u.src=ai.url||"//az416426.vo.msecnd.net/scripts/a/ai.0.js",t.getElementsByTagName(r)[0].parentNode.appendChild(u),ai.cookie=t.cookie,ai.queue=[],i=["Event","Exception","Metric","PageView","Trace"];i.length;)f("track"+i.pop());
+                return ai;
+            }({
+                iKey:"8d9c0114-1521-4d44-a19d-44e9df4897b3"
+            });
+
+            appInsights.trackPageView();
+        </script>
     </head>
     <body onload="load();">
         <?php include('analyticstracking.php'); ?>
