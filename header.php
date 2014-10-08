880cccd4aae9937c5f3a7043d11105e687984079
Brendan Boyle <brendancboyle@me.com>
2014-10-08 16:08:31 -0400

    Reduced header size

---
+++ header.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<!-- This site was written and developed by Brendan Boyle of DBZ Technology -->
+<!-- This site is being served through Microsoft Azure -->
+<html>
+    <head>
+        <title><?php echo 'DBZ Technology | '.$title; ?></title>
+        <?php include('includes.php'); ?>
+    </head>
+    <body onload="load();">
+        <?php include('analyticstracking.php'); ?>
+        <?php $index = false; include('nav.php'); ?>
+        <div class="content">
