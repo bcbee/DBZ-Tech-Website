0545830d110c759e44177d8bc81ace6c899db5bb
Brendan Boyle <brendancboyle@me.com>
2014-10-08 13:29:49 -0400

    Updated styling on nav

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
         <link rel="stylesheet" href="/CSS/bootstrap/css/bootstrap.min.css" />
         <link rel="stylesheet" href="/CSS/styles.css" />
-        <script async src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
+        <script src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
         <script async src="/CSS/bootstrap/js/bootstrap.min.js"></script>
         <script async src="/JS/scripts.js"></script>
         <script async src="/JS/index.js"></script>
