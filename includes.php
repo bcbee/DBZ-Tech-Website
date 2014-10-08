bd809da5e0f7a0647bf0305c06a0f36e00ed8197
Brendan Boyle <brendancboyle@me.com>
2014-10-08 16:08:20 -0400

    New nav outline, bug fixes, and less support

---
+++ includes.php
@@ -0,0 +1,21 @@
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<meta name="msvalidate.01" content="59E1BC42B9D918F982BBB8F8BF6E4F6F" />
+<link rel="stylesheet" href="/CSS/bootstrap/css/bootstrap.min.css" />
+<link rel="stylesheet/less" type="text/css" href="/CSS/styles.less" />
+<script src="//cdnjs.cloudflare.com/ajax/libs/less.js/1.7.5/less.min.js"></script>
+<script src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
+<script async src="/CSS/bootstrap/js/bootstrap.min.js"></script>
+<script async src="/JS/scripts.js"></script>
+<?php include('pingdom.php'); ?>
+<script type="text/javascript">
+    window.appInsights=window.appInsights||function(ai){
+        function f(t){ai[t]=function(){var i=arguments;ai.queue.push(function(){ai[t].apply(ai,i)})}}
+        var t=document,r="script",u=t.createElement(r),i;for(u.src=ai.url||"//az416426.vo.msecnd.net/scripts/a/ai.0.js",t.getElementsByTagName(r)[0].parentNode.appendChild(u),ai.cookie=t.cookie,ai.queue=[],i=["Event","Exception","Metric","PageView","Trace"];i.length;)f("track"+i.pop());
+        return ai;
+    }({
+        iKey:"c0ae0aae-2c08-44ad-aff3-d9b37246d4ea"
+    });
+
+    appInsights.trackPageView();
+</script>
---
+++ nav.php
@@ -0,0 +1,7 @@
+<div class="header" style="<?php if($index){echo('display: none;');} ?>">
+    <a href="#view4" class="glyphicon glyphicon-th mobilenav navmenu"></a>
+    <a href="/" class="logo contentlogo" data-toggle="tooltip" title="Home">
+        <h1>DBZ</h1>
+    </a>
+    <?php include('navbuttons.php'); ?>
+</div>
