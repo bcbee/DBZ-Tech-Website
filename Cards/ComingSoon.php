c329b9bff4a80afb13b52588413d249d927117fb
Brendan Boyle <brendancboyle@me.com>
2019-12-24 03:53:14 -0600

    Update ComingSoon.php

--- Cards/ComingSoon.php
+++ Cards/ComingSoon.php
@@ -1,2 +1,2 @@
 <h1>Coming Soon!</h1>
-<p>This website is still under development! Check back soon for more info on DBZ Technology</p>
+<p>This website is still under development! Check back soon for more info on DBZ Technology.</p>
