53e866d3b9a0a811e7b1cdd51d7032600a9d6a63
Brendan Boyle <brendancboyle@me.com>
2021-10-27 18:23:41 -0400

    Add privacy page

---
+++ Privacy.php
@@ -0,0 +1,92 @@
+<?php $title = "Home"; ?>
+<!doctype html>
+<!-- BETA VERSION -->
+<!-- This site was written and developed by Brendan Boyle of DBZ Technology -->
+<html>
+
+<head>
+    <title>DBZ Technology | Home</title>
+    <?php include('includes.php'); ?>
+    <script async src="/JS/index.js"></script>
+</head>
+
+<body onload="load();">
+    <h2>DBZ Technology LLC Terms of Service and Privacy Policy</h2>
+    <br>
+    <h3>1. Terms</h3>
+
+    <p>By accessing the website and mobile application at <a href="https://dbztech.com">https://dbztech.com</a>, you are agreeing to be bound by these terms of service, all applicable laws and regulations, and agree that you are responsible for compliance with any applicable local laws. If you do not agree with any of these terms, you are prohibited from using or accessing this site. The materials contained in this website and mobile application are protected by applicable copyright and trademark law.</p>
+
+    <br>
+
+    <h3>2. Use License</h3>
+
+    <ol type="a">
+    <li>
+    Permission is granted to temporarily download one copy of the materials (information or software) on DBZ Technology LLC's website and mobile application for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license you may not:
+
+    <ol type="i">
+    <li>modify or copy the materials;</li>
+    <li>use the materials for any commercial purpose, or for any public display (commercial or non-commercial);</li>
+    <li>attempt to decompile or reverse engineer any software contained on DBZ Technology LLC's website and mobile application;</li>
+    <li>remove any copyright or other proprietary notations from the materials; or</li>
+    <li>transfer the materials to another person or "mirror" the materials on any other server.</li>
+    </ol>
+    </li>
+    <li>This license shall automatically terminate if you violate any of these restrictions and may be terminated by DBZ Technology LLC at any time. Upon terminating your viewing of these materials or upon the termination of this license, you must destroy any downloaded materials in your possession whether in electronic or printed format.</li>
+    </ol>
+    <br>
+    <h3>3. Disclaimer</h3>
+
+    <ol type="a">
+    <li>The materials on DBZ Technology LLC's website and mobile application are provided on an 'as is' basis. DBZ Technology LLC makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties including, without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights.</li>
+    <li>Further, DBZ Technology LLC does not warrant or make any representations concerning the accuracy, likely results, or reliability of the use of the materials on its website and mobile application or otherwise relating to such materials or on any sites linked to this site.</li>
+    </ol>
+
+    <br>
+
+    <h3>4. Limitations</h3>
+
+    <p>In no event shall DBZ Technology LLC or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption) arising out of the use or inability to use the materials on DBZ Technology LLC's website and mobile application, even if DBZ Technology LLC or a DBZ Technology LLC authorized representative has been notified orally or in writing of the possibility of such damage. Because some jurisdictions do not allow limitations on implied warranties, or limitations of liability for consequential or incidental damages, these limitations may not apply to you.</p>
+
+    <br>
+    <h3>5. Accuracy of materials</h3>
+
+    <p>The materials appearing on DBZ Technology LLC's website and mobile application could include technical, typographical, or photographic errors. DBZ Technology LLC does not warrant that any of the materials on its website and mobile application are accurate, complete or current. DBZ Technology LLC may make changes to the materials contained on its website and mobile application at any time without notice. However DBZ Technology LLC does not make any commitment to update the materials.</p>
+
+    <br>
+    <h3>6. Links</h3>
+
+    <p>DBZ Technology LLC has not reviewed all of the sites linked to its website and mobile application and is not responsible for the contents of any such linked site. The inclusion of any link does not imply endorsement by DBZ Technology LLC of the site. Use of any such linked website and mobile application is at the user's own risk.</p>
+
+    <br>
+    <h3>7. Modifications</h3>
+
+    <p>DBZ Technology LLC may revise these terms of service for its website and mobile application at any time without notice. By using this website and mobile application you are agreeing to be bound by the then current version of these terms of service.</p>
+    <br>
+
+    <h3>8. Governing Law</h3>
+
+    <p>These terms and conditions are governed by and construed in accordance with the laws of Minnesota and you irrevocably submit to the exclusive jurisdiction of the courts in that State or location.</p>
+
+    <br>
+    <h2>Privacy Policy</h2>
+
+    <p>Your privacy is important to us.</p>
+
+    <p>It is DBZ Technology LLC's policy to respect your privacy regarding any information we may collect while operating our website and mobile application. Accordingly, we have developed this privacy policy in order for you to understand how we collect, use, communicate, disclose and otherwise make use of personal information. We have outlined our privacy policy below.</p>
+
+    <ul>
+    <li>We will collect personal information by lawful and fair means and, where appropriate, with the knowledge or consent of the individual concerned.</li>
+    <li>Before or at the time of collecting personal information, we will identify the purposes for which information is being collected.</li>
+    <li>We will collect and use personal information solely for fulfilling those purposes specified by us and for other ancillary purposes, unless we obtain the consent of the individual concerned or as required by law.</li>
+    <li>Personal data should be relevant to the purposes for which it is to be used, and, to the extent necessary for those purposes, should be accurate, complete, and up-to-date.</li>
+    <li>We will protect personal information by using reasonable security safeguards against loss or theft, as well as unauthorized access, disclosure, copying, use or modification.</li>
+    <li>We will make readily available to customers information about our policies and practices relating to the management of personal information.</li>
+    <li>We will only retain personal information for as long as necessary for the fulfilment of those purposes.</li>
+    </ul>
+
+    <p>We are committed to conducting our business in accordance with these principles in order to ensure that the confidentiality of personal information is protected and maintained. DBZ Technology LLC may change this privacy policy from time to time at DBZ Technology LLC's sole discretion.</p>
+</body>
+
+</html>
